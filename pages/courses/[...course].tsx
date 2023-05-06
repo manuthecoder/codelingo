52b081d0a0363e08721e5bf3c27659485097858f
_manu.codes <manusvathgurudath@gmail.com>
2023-05-06 21:36:15 +0000

    W

--- pages/courses/[...course].tsx
+++ pages/courses/[...course].tsx
@@ -1,7 +1,9 @@
 import { Layout } from "@/components/Layout";
 
 export default function CoursePage() {
-  const getRandomLesson = () => {};
+  const getRandomLesson = () => {
+    
+  };
 
   return <Layout>no</Layout>;
 }
