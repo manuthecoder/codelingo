dc03fe775b7d45d68cf2167df519916aadbe072d
_manu.codes <manusvathgurudath@gmail.com>
2023-05-06 21:36:09 +0000

    W

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-	return (
-		<Html lang="en">
-			<Head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Foldit:wght@600&family=Work+Sans:wght@300;500;700;900&display=swap"
-					rel="stylesheet"
-				/>
-			</Head>
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Foldit:wght@600&family=Work+Sans:wght@300;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="theme-color" content="hsl(198, 45%, 90%)" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
