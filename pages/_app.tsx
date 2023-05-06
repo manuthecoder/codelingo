3a8135b2001446971ef7c1a0e3f9927004cb4cd8
_manu.codes <manusvathgurudath@gmail.com>
2023-05-06 18:22:22 +0000

    W

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Codelingo</title>
+        <link rel="shortcut icon" href="/icon/48x48.png" />
+        <meta name="description" content="Your Interactive Coding Companion" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+        <link rel="shortcut icon" href="/icon/48x48.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
